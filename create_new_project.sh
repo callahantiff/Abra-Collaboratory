00b54c9491edb60ce582b8ac7dd971832060de1b
tuh8888 <harrison.pielke-lombardo@ucdenver.edu>
2019-05-13 17:17:52 -0600

    Ask for username and project name before asking to continue

--- create_new_project.sh
+++ create_new_project.sh
@@ -4,17 +4,17 @@ echo "Before running this script you must:"
 echo "   1) Create a new repository to serve as your Abra-Collaboratory framework based repository"
 echo "   2) Create the first wiki page in your repository manually."
 
+if [ $# != 2 ]
+  then
+    echo "Please provide user name and project name"
+    exit 1
+fi
+
 read -p "Are you ready to continue (y/n)? " -n 1 -r
 echo    # (optional) move to a new line
 if [[ ! $REPLY =~ ^[Yy]$ ]]
 then
     [[ "$0" = "$BASH_SOURCE" ]] && exit 1 || return 1 # handle exits from shell or function but don't exit interactive shell
-fi
-
-if [ $# != 2 ]
-  then
-    echo "Please provide user name and project name"
-    exit 1
 fi
 
 USER_NAME=$1
